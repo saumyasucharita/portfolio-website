16a9c45f80ca0e16bd84d46493e8fc7bc8f6a290
saumya <saumya.sucharita@gmail.com>
2023-05-17 18:57:48 -0700

    Added bash script for static site generator

---
+++ build.sh
@@ -0,0 +1,4 @@
+cat ./templates/top.html ./content/index.html ./templates/bottom.html > docs/index.html
+cat ./templates/top.html ./content/education.html ./templates/bottom.html > docs/education.html
+cat ./templates/top.html ./content/experience.html ./templates/bottom.html > docs/experience.html
+cat ./templates/top.html ./content/projects.html ./templates/bottom.html > docs/projects.html
